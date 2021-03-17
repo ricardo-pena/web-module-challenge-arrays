996df7e7455404fafc45ee2ed286b072d4b9fc2e
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-03-17 13:51:04 -0400

    Finished task 6 and 7

--- index.js
+++ index.js
@@ -134,11 +134,18 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
+function removeFlavorByName(array, flavor){
+
     /*your code here*/
+    for(let i =0; i< array.length;i++){
+        if(array[i]=== flavor){
+            array.splice(i,1);
+        }
+    }
+    return array;
 }
 
-
+console.log('task 6:', removeFlavorByName(originalFlavors,'Rocky Road'));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. 
@@ -160,8 +167,16 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
+function filterByWord(array, string){
     /*your code here*/
+    const filteredArray =[];
+    for (let i = 0; i< array.length; i++){
+        if(array[i].includes(string)){
+            filteredArray.push(array[i]);
+        }
+
+    }
+    return filteredArray;
 }
 
 
